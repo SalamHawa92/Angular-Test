b0c0937d06c3602e75795c50cbe038e7f90f7603
salam <salamabuelhawa@gmail.com>
2020-12-17 15:30:27 +0200

    now second component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
+import { HerosComponent } from './heros/heros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNewComponentComponent,
+    HerosComponent
   ],
   imports: [
     BrowserModule
