9e3a40570791d9d49ce4e82fd44a6baec3d996bc
salam <salamabuelhawa@gmail.com>
2020-12-23 11:40:16 +0200

    before mock heros

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from '../hero';
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
   styleUrls: ['./heros.component.css']
 })
 export class HerosComponent implements OnInit {
+
+  /*hero = 'winstorm' ;*/
+  hero: Hero = {
+  id : 1,
+  name : 'windstorm'
+  };
 
   constructor() { }
 
